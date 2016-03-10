ad7f14022bb86db43a48036cd246f3737a7e4430
Michel Michaux <michaux.michel@gmail.com>
2016-03-10 16:08:18 +0100

    Tryout admin laten overzicht css + addin icons

---
+++ _temp/index.php
@@ -0,0 +1,38 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin lijst concept</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<!--Header - menu-->
+<header class="container-fluid">
+    <nav class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <ul>
+                    <li><a href="admin.html">Planten</a></li>
+                    <li><a href="admin.html">Toevoegen</a></li>
+                    <li class="logout"><a href="admin.html">Uitloggen</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+
+
+<!--Planten lijst-->
+<div class="container">
+    <article id="content">
+
+    </article>
+</div>
+
+
+</body>
+</html>
